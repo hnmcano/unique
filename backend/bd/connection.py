f8e5b441f42a8cb76bded1053484315604c4ed63
hnmcano <harison.natan014@outlook.com>
2025-12-21 21:28:12 -0300

    Alteração no create_engine

--- backend/bd/connection.py
+++ backend/bd/connection.py
@@ -7,7 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
