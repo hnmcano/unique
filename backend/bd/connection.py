c8eec1c0760780724c96cabd44fa59f367c16127
hnmcano <harison.natan014@outlook.com>
2025-12-22 21:58:09 -0300

    Alteração na conexão

--- backend/bd/connection.py
+++ backend/bd/connection.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./uniqueDatabase.db")
@@ -7,8 +8,17 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./uniqueDatabase.db")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool,
+    pool_pre_ping=True,
+    connect_args={"SSLmode": "require"} if DATABASE_URL.startswith("postgresql://") else {}
+                       )
+
+SessionLocal = sessionmaker(
+    autocommit=False, 
+    autoflush=False, 
+    bind=engine)
 
 Base = declarative_base()
 
