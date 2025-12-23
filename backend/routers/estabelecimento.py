35937505de26d677b1b8e666c4773b848d432997
hnmcano <harison.natan014@outlook.com>
2025-12-22 23:39:12 -0300

    Deploy rota /estabelecimento/react/catalago

--- backend/routers/estabelecimento.py
+++ backend/routers/estabelecimento.py
@@ -4,11 +4,10 @@ from sqlalchemy.orm import Session
 from models.estabelecimento import Estabelecimento as EstabelecimentoModel
 from schemas.estabelecimento import EstabelecimentoBase as Estabelecimentoschema
 
-
 router = APIRouter()
 
 @router.post("/desktop/add")
-def info_data_estabelecimento(dados_estabelecimento: Estabelecimentoschema, db: Session = Depends(get_db)):
+async def info_data_estabelecimento(dados_estabelecimento: Estabelecimentoschema, db: Session = Depends(get_db)):
     db_estabelecimento = EstabelecimentoModel(**dados_estabelecimento.dict())
 
     db_estabelecimento_valid = db.query(EstabelecimentoModel).filter(EstabelecimentoModel.nome == dados_estabelecimento.nome).first()
@@ -23,7 +22,7 @@ def info_data_estabelecimento(dados_estabelecimento: Estabelecimentoschema, db: 
 
 
 @router.put("/desktop/update/{estabelecimento_nome}")
-def update_estabelecimento(estabelecimento_nome: str, dados_estabelecimento: Estabelecimentoschema, db: Session = Depends(get_db)):
+async def update_estabelecimento(estabelecimento_nome: str, dados_estabelecimento: Estabelecimentoschema, db: Session = Depends(get_db)):
     db_estabelecimento = db.query(EstabelecimentoModel).filter(EstabelecimentoModel.nome == estabelecimento_nome).first()
 
     for key, value in dados_estabelecimento.dict().items():
@@ -34,8 +33,11 @@ def update_estabelecimento(estabelecimento_nome: str, dados_estabelecimento: Est
     return db_estabelecimento
 
 @router.get("/react/catalago")
-def get_estabelecimento( db: Session = Depends(get_db)):
-    db_estabelecimento = db.query(EstabelecimentoModel).all()
+async def get_estabelecimento(db: Session = Depends(get_db)):
+
+    db_estabelecimento = db.query(EstabelecimentoModel).first()
+    print(db_estabelecimento)
+
     if not db_estabelecimento:
         raise HTTPException(status_code=404, detail="Estabelecimento não encontrado")
     
