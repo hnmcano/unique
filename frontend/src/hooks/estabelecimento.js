f758821bbfeed3fc839dc213739011676ad10f5e
hnmcano <harison.natan014@outlook.com>
2025-12-22 23:58:54 -0300

    Corrigindo outro erro no estabekecimento.js

--- frontend/src/hooks/estabelecimento.js
+++ frontend/src/hooks/estabelecimento.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import api from "../api/api";
 
 
-const useEstabelecimento = () => {
+export const useEstabelecimento = () => {
     const [ BaseEstabelecimento, setBaseEstabelecimento] = useState([]);
 
     useEffect(() => {
