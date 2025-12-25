0af0c331b34f3b7fbcdfbef06ec5020c187e8263
hnmcano <harison.natan014@outlook.com>
2025-12-25 17:33:37 -0300

    correção container background

--- desktop/window/form_products/add_produtos_ui.py
+++ desktop/window/form_products/add_produtos_ui.py
@@ -15,16 +15,17 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QGridLayout,
-    QHBoxLayout, QLabel, QLineEdit, QMainWindow,
-    QPushButton, QSizePolicy, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QButtonGroup, QComboBox, QFrame,
+    QGridLayout, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QPlainTextEdit, QPushButton, QRadioButton,
+    QSizePolicy, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(659, 763)
-        MainWindow.setMinimumSize(QSize(620, 698))
+        MainWindow.resize(646, 546)
+        MainWindow.setMinimumSize(QSize(0, 0))
         MainWindow.setMaximumSize(QSize(1731691, 16777215))
         MainWindow.setStyleSheet(u"")
         self.centralwidget = QWidget(MainWindow)
@@ -34,40 +35,6 @@ class Ui_MainWindow(object):
         self.gridLayout.setSpacing(0)
         self.gridLayout.setObjectName(u"gridLayout")
         self.gridLayout.setContentsMargins(0, 0, 0, 0)
-        self.widget_5 = QWidget(self.centralwidget)
-        self.widget_5.setObjectName(u"widget_5")
-        self.widget_5.setMaximumSize(QSize(16777215, 30))
-        self.widget_5.setStyleSheet(u"")
-
-        self.gridLayout.addWidget(self.widget_5, 6, 0, 1, 2)
-
-        self.widget = QWidget(self.centralwidget)
-        self.widget.setObjectName(u"widget")
-        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
-        self.widget.setSizePolicy(sizePolicy)
-        self.widget.setMaximumSize(QSize(16777215, 90))
-        self.widget.setStyleSheet(u"")
-        self.verticalLayout = QVBoxLayout(self.widget)
-        self.verticalLayout.setSpacing(0)
-        self.verticalLayout.setObjectName(u"verticalLayout")
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.label_14 = QLabel(self.widget)
-        self.label_14.setObjectName(u"label_14")
-        font = QFont()
-        font.setFamilies([u"Monotype Corsiva"])
-        font.setPointSize(45)
-        self.label_14.setFont(font)
-        self.label_14.setStyleSheet(u"")
-        self.label_14.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        self.verticalLayout.addWidget(self.label_14)
-
-
-        self.gridLayout.addWidget(self.widget, 0, 0, 1, 2)
-
         self.widget_4 = QWidget(self.centralwidget)
         self.widget_4.setObjectName(u"widget_4")
         self.widget_4.setMinimumSize(QSize(300, 0))
@@ -97,6 +64,9 @@ class Ui_MainWindow(object):
 
         self.frame_2 = QFrame(self.widget_4)
         self.frame_2.setObjectName(u"frame_2")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.frame_2.sizePolicy().hasHeightForWidth())
         self.frame_2.setSizePolicy(sizePolicy)
         self.frame_2.setStyleSheet(u"")
@@ -139,7 +109,7 @@ class Ui_MainWindow(object):
         self.gridLayout_3.addWidget(self.frame_3, 0, 0, 1, 3)
 
 
-        self.gridLayout.addWidget(self.widget_4, 4, 1, 1, 1)
+        self.gridLayout.addWidget(self.widget_4, 3, 1, 1, 1)
 
         self.widget_3 = QWidget(self.centralwidget)
         self.widget_3.setObjectName(u"widget_3")
@@ -161,7 +131,7 @@ class Ui_MainWindow(object):
         self.gridLayout_4.addWidget(self.cancelar, 0, 1, 1, 1)
 
 
-        self.gridLayout.addWidget(self.widget_3, 5, 0, 1, 2)
+        self.gridLayout.addWidget(self.widget_3, 4, 0, 1, 2)
 
         self.widget_2 = QWidget(self.centralwidget)
         self.widget_2.setObjectName(u"widget_2")
@@ -169,37 +139,16 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setSpacing(0)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.gridLayout_2.setContentsMargins(9, 9, 0, 0)
-        self.preco_custo_input = QLineEdit(self.widget_2)
-        self.preco_custo_input.setObjectName(u"preco_custo_input")
-        self.preco_custo_input.setMaximumSize(QSize(240, 16777215))
+        self.nome_input = QLineEdit(self.widget_2)
+        self.nome_input.setObjectName(u"nome_input")
+        self.nome_input.setMaximumSize(QSize(240, 16777215))
 
-        self.gridLayout_2.addWidget(self.preco_custo_input, 9, 2, 1, 1)
-
-        self.label_2 = QLabel(self.widget_2)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout_2.addWidget(self.label_2, 7, 0, 1, 1)
-
-        self.categoria_combo = QComboBox(self.widget_2)
-        self.categoria_combo.setObjectName(u"categoria_combo")
-        self.categoria_combo.setMaximumSize(QSize(240, 16777215))
-
-        self.gridLayout_2.addWidget(self.categoria_combo, 7, 2, 1, 1)
-
-        self.label = QLabel(self.widget_2)
-        self.label.setObjectName(u"label")
-
-        self.gridLayout_2.addWidget(self.label, 2, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.nome_input, 8, 2, 1, 1)
 
         self.label_6 = QLabel(self.widget_2)
         self.label_6.setObjectName(u"label_6")
 
         self.gridLayout_2.addWidget(self.label_6, 8, 0, 1, 1)
-
-        self.label_5 = QLabel(self.widget_2)
-        self.label_5.setObjectName(u"label_5")
-
-        self.gridLayout_2.addWidget(self.label_5, 13, 0, 1, 1)
 
         self.preco_venda_input = QLineEdit(self.widget_2)
         self.preco_venda_input.setObjectName(u"preco_venda_input")
@@ -207,41 +156,21 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.preco_venda_input, 10, 2, 1, 1)
 
+        self.label = QLabel(self.widget_2)
+        self.label.setObjectName(u"label")
+
+        self.gridLayout_2.addWidget(self.label, 2, 0, 1, 1)
+
         self.estoque_min_input = QLineEdit(self.widget_2)
         self.estoque_min_input.setObjectName(u"estoque_min_input")
         self.estoque_min_input.setMaximumSize(QSize(240, 16777215))
 
         self.gridLayout_2.addWidget(self.estoque_min_input, 13, 2, 1, 1)
 
-        self.label_3 = QLabel(self.widget_2)
-        self.label_3.setObjectName(u"label_3")
-        self.label_3.setMaximumSize(QSize(130, 16777215))
+        self.label_5 = QLabel(self.widget_2)
+        self.label_5.setObjectName(u"label_5")
 
-        self.gridLayout_2.addWidget(self.label_3, 9, 0, 1, 1)
-
-        self.cod_pdv_input = QLineEdit(self.widget_2)
-        self.cod_pdv_input.setObjectName(u"cod_pdv_input")
-        self.cod_pdv_input.setMaximumSize(QSize(240, 16777215))
-
-        self.gridLayout_2.addWidget(self.cod_pdv_input, 2, 2, 1, 1)
-
-        self.Estoque_input = QLineEdit(self.widget_2)
-        self.Estoque_input.setObjectName(u"Estoque_input")
-        self.Estoque_input.setMaximumSize(QSize(240, 16777215))
-
-        self.gridLayout_2.addWidget(self.Estoque_input, 12, 2, 1, 1)
-
-        self.desc_input = QLineEdit(self.widget_2)
-        self.desc_input.setObjectName(u"desc_input")
-        self.desc_input.setMinimumSize(QSize(240, 130))
-        self.desc_input.setMaximumSize(QSize(240, 16777215))
-
-        self.gridLayout_2.addWidget(self.desc_input, 14, 2, 1, 1)
-
-        self.label_8 = QLabel(self.widget_2)
-        self.label_8.setObjectName(u"label_8")
-
-        self.gridLayout_2.addWidget(self.label_8, 11, 0, 1, 1)
+        self.gridLayout_2.addWidget(self.label_5, 13, 0, 1, 1)
 
         self.label_13 = QLabel(self.widget_2)
         self.label_13.setObjectName(u"label_13")
@@ -249,40 +178,100 @@ class Ui_MainWindow(object):
 
         self.gridLayout_2.addWidget(self.label_13, 14, 0, 1, 1)
 
-        self.nome_input = QLineEdit(self.widget_2)
-        self.nome_input.setObjectName(u"nome_input")
-        self.nome_input.setMaximumSize(QSize(240, 16777215))
+        self.label_2 = QLabel(self.widget_2)
+        self.label_2.setObjectName(u"label_2")
 
-        self.gridLayout_2.addWidget(self.nome_input, 8, 2, 1, 1)
+        self.gridLayout_2.addWidget(self.label_2, 7, 0, 1, 1)
 
-        self.Medida_input = QLineEdit(self.widget_2)
-        self.Medida_input.setObjectName(u"Medida_input")
-        self.Medida_input.setMaximumSize(QSize(240, 16777215))
+        self.preco_custo_input = QLineEdit(self.widget_2)
+        self.preco_custo_input.setObjectName(u"preco_custo_input")
+        self.preco_custo_input.setMaximumSize(QSize(240, 16777215))
 
-        self.gridLayout_2.addWidget(self.Medida_input, 11, 2, 1, 1)
+        self.gridLayout_2.addWidget(self.preco_custo_input, 9, 2, 1, 1)
+
+        self.categoria_combo = QComboBox(self.widget_2)
+        self.categoria_combo.setObjectName(u"categoria_combo")
+        self.categoria_combo.setMaximumSize(QSize(240, 16777215))
+
+        self.gridLayout_2.addWidget(self.categoria_combo, 7, 2, 1, 1)
+
+        self.label_3 = QLabel(self.widget_2)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMaximumSize(QSize(130, 16777215))
+
+        self.gridLayout_2.addWidget(self.label_3, 9, 0, 1, 1)
+
+        self.Estoque_input = QLineEdit(self.widget_2)
+        self.Estoque_input.setObjectName(u"Estoque_input")
+        self.Estoque_input.setMaximumSize(QSize(240, 16777215))
+
+        self.gridLayout_2.addWidget(self.Estoque_input, 12, 2, 1, 1)
 
         self.label_4 = QLabel(self.widget_2)
         self.label_4.setObjectName(u"label_4")
 
         self.gridLayout_2.addWidget(self.label_4, 12, 0, 1, 1)
 
+        self.label_8 = QLabel(self.widget_2)
+        self.label_8.setObjectName(u"label_8")
+
+        self.gridLayout_2.addWidget(self.label_8, 11, 0, 1, 1)
+
+        self.cod_pdv_input = QLineEdit(self.widget_2)
+        self.cod_pdv_input.setObjectName(u"cod_pdv_input")
+        self.cod_pdv_input.setMaximumSize(QSize(240, 16777215))
+
+        self.gridLayout_2.addWidget(self.cod_pdv_input, 2, 2, 1, 1)
+
+        self.desc_input = QPlainTextEdit(self.widget_2)
+        self.desc_input.setObjectName(u"desc_input")
+        self.desc_input.setMaximumSize(QSize(240, 150))
+
+        self.gridLayout_2.addWidget(self.desc_input, 14, 2, 1, 1)
+
         self.label_12 = QLabel(self.widget_2)
         self.label_12.setObjectName(u"label_12")
 
         self.gridLayout_2.addWidget(self.label_12, 10, 0, 1, 1)
 
+        self.radioWidget = QWidget(self.widget_2)
+        self.radioWidget.setObjectName(u"radioWidget")
+        self.radioWidget.setMaximumSize(QSize(240, 35))
+        self.horizontalLayout_4 = QHBoxLayout(self.radioWidget)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.radioButton = QRadioButton(self.radioWidget)
+        self.buttonGroup = QButtonGroup(MainWindow)
+        self.buttonGroup.setObjectName(u"buttonGroup")
+        self.buttonGroup.addButton(self.radioButton)
+        self.radioButton.setObjectName(u"radioButton")
 
-        self.gridLayout.addWidget(self.widget_2, 4, 0, 1, 1)
+        self.horizontalLayout_4.addWidget(self.radioButton)
+
+        self.radioButton_2 = QRadioButton(self.radioWidget)
+        self.buttonGroup.addButton(self.radioButton_2)
+        self.radioButton_2.setObjectName(u"radioButton_2")
+
+        self.horizontalLayout_4.addWidget(self.radioButton_2)
+
+        self.radioButton_3 = QRadioButton(self.radioWidget)
+        self.buttonGroup.addButton(self.radioButton_3)
+        self.radioButton_3.setObjectName(u"radioButton_3")
+
+        self.horizontalLayout_4.addWidget(self.radioButton_3)
+
+
+        self.gridLayout_2.addWidget(self.radioWidget, 11, 2, 1, 1)
+
+
+        self.gridLayout.addWidget(self.widget_2, 3, 0, 1, 1)
 
         MainWindow.setCentralWidget(self.centralwidget)
         QWidget.setTabOrder(self.cod_pdv_input, self.nome_input)
         QWidget.setTabOrder(self.nome_input, self.preco_custo_input)
         QWidget.setTabOrder(self.preco_custo_input, self.preco_venda_input)
-        QWidget.setTabOrder(self.preco_venda_input, self.Medida_input)
-        QWidget.setTabOrder(self.Medida_input, self.Estoque_input)
+        QWidget.setTabOrder(self.preco_venda_input, self.Estoque_input)
         QWidget.setTabOrder(self.Estoque_input, self.estoque_min_input)
-        QWidget.setTabOrder(self.estoque_min_input, self.desc_input)
-        QWidget.setTabOrder(self.desc_input, self.selecionar_imagem)
+        QWidget.setTabOrder(self.estoque_min_input, self.selecionar_imagem)
         QWidget.setTabOrder(self.selecionar_imagem, self.limp_img)
         QWidget.setTabOrder(self.limp_img, self.add_produto)
         QWidget.setTabOrder(self.add_produto, self.cancelar)
@@ -294,21 +283,23 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"CADASTRO DE PRODUTOS", None))
-        self.label_14.setText(QCoreApplication.translate("MainWindow", u"Cadastro de Produtos", None))
         self.selecionar_imagem.setText(QCoreApplication.translate("MainWindow", u"Selecionar imagem", None))
         self.limp_img.setText(QCoreApplication.translate("MainWindow", u"Excluir Foto", None))
         self.image_label.setText("")
         self.add_categoria.setText(QCoreApplication.translate("MainWindow", u"ADD CATEGORIA", None))
         self.add_produto.setText(QCoreApplication.translate("MainWindow", u"Adicionar Produto", None))
         self.cancelar.setText(QCoreApplication.translate("MainWindow", u"Cancelar", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Categoria", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"C\u00f3d. PDV", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Nome", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"C\u00f3d. PDV", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"Estoque m\u00edn", None))
-        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Pre\u00e7o Custo", None))
-        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Medida", None))
         self.label_13.setText(QCoreApplication.translate("MainWindow", u"Descri\u00e7\u00e3o", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Categoria", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Pre\u00e7o Custo", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Estoque", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"Medida", None))
         self.label_12.setText(QCoreApplication.translate("MainWindow", u"Pre\u00e7o Venda", None))
+        self.radioButton.setText(QCoreApplication.translate("MainWindow", u"UN", None))
+        self.radioButton_2.setText(QCoreApplication.translate("MainWindow", u"KG", None))
+        self.radioButton_3.setText(QCoreApplication.translate("MainWindow", u"GR", None))
     # retranslateUi
 
