896fa08a672fd6b2e6063c186c4522dcd025bbbe
hnmcano <harison.natan014@outlook.com>
2026-01-02 18:02:10 -0300

    atualização carrinho, produtos

--- frontend/src/components/Map_produtos.jsx
+++ frontend/src/components/Map_produtos.jsx
@@ -1,9 +1,11 @@
 import ProdutoData from "./ProdutoData";
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, use} from "react";
+import { useCarrinho } from "../hooks/useCarrinho";
 
 function ProdutosAcionados({categoria}) {
     const [modalOpenProduto, setModalOpenProduto] = useState(false);
     const [produtoSelecionado, setProdutoSelecionado] = useState(null);
+    const { quantidadeItems } = useCarrinho();
 
     const imagemUrl = (imagem) => `data:image/png;base64,${imagem}`;
 
@@ -16,7 +18,7 @@ function ProdutosAcionados({categoria}) {
         setModalOpenProduto(false);
         setProdutoSelecionado(null);
     };
-        
+
     return (    
         <div className="products-gouped">             
             <ProdutoData
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,13 +2,13 @@
 import ContainerCategories from "../components/Catalago";
 import CadastrarUser from "../components/User";
 //import Options from "../components/Options";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/HomePage.css"
 import { CiBoxList } from "react-icons/ci";
 import { GiShoppingCart } from "react-icons/gi";
 import { FaRegUserCircle } from "react-icons/fa";
 import ModalShopping from "../components/Carrinho";
-import Footer from "../components/Footer";
+
 
 // Função da pagina principal
 function HomePage(){
@@ -19,6 +19,7 @@ function HomePage(){
 
     const openModal = () => setModalShoppingOpen(true);
     const closeModal = () => setModalShoppingOpen(false);
+
 
     return (
     <div className="home">
