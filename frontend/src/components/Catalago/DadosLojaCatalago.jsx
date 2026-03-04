e6baad0f386da809b6c9ad684e7ea21c8a63a3ae
hnmcano <harison.natan014@outlook.com>
2026-03-04 11:10:39 -0300

    atualização dados da loja

--- frontend/src/components/Catalago/DadosLojaCatalago.jsx
+++ frontend/src/components/Catalago/DadosLojaCatalago.jsx
@@ -11,25 +11,27 @@ function DadosLojaCatalago({}) {
                 <img className="logotipo" src={`data:image/png;base64,${estabelecimento?.logo_img}`} alt="Logo"/>
             </div>
             <div>
-                <div class="status-badge">ABERTO</div>
+                <div class="status-container">
+                    <label className={status === "ABERTO" ? "status_aberto" : "status_fechado"}>{status}</label>
+                </div>
                 <header>
                     <h1>Hookah Shisha</h1>
-                    <p class="subtitle">Tabacaria & Lounge</p>
+                    <p class="subtitle">Tabacaria</p>
                 </header>
                 <div class="info-container">
                     <div class="info-item">
-                    <span class="label">Endereço</span>
-                    <span class="value accent">ONLINE</span>
+                        <span className="montserrat-data">Endereço: {estabelecimento?.endereco}</span>
+                        <span class="value accent">ONLINE</span>
                     </div>
                     
                     <div class="info-item">
-                    <span class="label">Horário</span>
-                    <span class="value">13:00 às 17:00</span>
+                        <span class="label">Horário</span>
+                        <span class="value">13:00 às 17:00</span>
                     </div>
                 </div>
                 <footer class="contact-bar">
-                    <a href="https://wa.me/5514996323908" class="contact-link">📞 (14) 99632-3908</a>
-                    <a href="#" class="contact-link">📸 @HookahshishaTabacaria</a>
+                    <a href="https://wa.me/5514996323908" class="contact-link">📞 {estabelecimento?.telefone}</a>
+                    <a href="#" class="contact-link">📸 {estabelecimento?.rede_social}</a>
                 </footer>
             </div>
         </>
