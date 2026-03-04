413c5a1ce8c952ca5e958ffb2d23a945d0b64459
hnmcano <harison.natan014@outlook.com>
2026-03-04 16:33:20 -0300

    Atualização envio de pagamento

--- frontend/src/components/Checkout/FormasPagPedido.jsx
+++ frontend/src/components/Checkout/FormasPagPedido.jsx
@@ -43,116 +43,105 @@ function FormasPagPedido() {
             console.log(error);
             setIsLoading(false);
         }
-        return (
-            <>
-                {isLoading && <div className="fullscreen-loading" />}
-
-                <button
-                    onClick={handledSubmit}
-                    className={`send 
-                        ${isLoading ? "is-loading" : ""} 
-                        ${isSuccess ? "is-success" : ""}`}
-                >
-                    <svg viewBox="0 0 90.594 59.714">
-                        <polyline
-                            className="check"
-                            fill="none"
-                            stroke="#FFFFFF"
-                            strokeWidth="10"
-                            strokeMiterlimit="10"
-                            points="1.768,23.532 34.415,56.179 88.826,1.768"
-                        />
-                    </svg>
-                    <span>Send</span>
-                </button>
-            </>
-        );
 
     }
     
     return (
         <>
-        <div className="metodos-pagamento-container">
-            <div className="formas-pagamentos">
-                <div className="opcao-pagamento" >
-                    <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("pix")} >
-                        <IconePix onClick={() => ToogleVisibility("pix")}/>
-                        <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("pix")}>PIX</span>
-                        <input
-                            type="radio"
-                            name="pagamento"
-                            value="pix"
-                            style={{ display: "none"}}
-                            checked={formaPagamento === "pix"}
-                            onChange={() => handleSelect("pix")}
-                        />
-                    </label>
-                    {opcoesDisponiveis === "pix" && (<PagamentoPix />)}
-                </div>
-               <div className="opcao-pagamento" >
-                    <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("credito")}>
-                        <IconeCredito onClick={() => ToogleVisibility("credito")}/>
-                        <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("credito")}>CRÉDITO</span>
-                        <input
-                            type="radio"
-                            name="pagamento"
-                            value="credito"
-                            style={{ display: "none"}}
-                            checked={formaPagamento === "credito"}
-                            onChange={() => handleSelect("credito")}
-                        />
-                    </label>
-                    {opcoesDisponiveis === "credito" && (<PagamentoCredito />)}
-                </div>
-                <div className="opcao-pagamento" >
-                    <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("debito")}>
-                        <IconeDebito onClick={() => ToogleVisibility("debito")} />
-                        <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("debito")}>DÉBITO</span>
-                        <input
-                            type="radio"
-                            name="pagamento"
-                            value="debito"
-                            style={{ display: "none"}}
-                            checked={formaPagamento === "debito"}
-                            onChange={() => handleSelect("debito")}
-                        />
-                    </label>
-                    {opcoesDisponiveis === "debito" && (<PagamentoDebito />)}
-                </div>
-                <div className="opcao-pagamento" >
-                    <div className="checkbox-pagamentos" onClick={() => ToogleVisibility("dinheiro")}>
-                        <IconeDinheiro onClick={() => ToogleVisibility("dinheiro")}/>
-                        <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("dinheiro")}>DINHEIRO</span>
-                        <input
-                            type="radio"
-                            name="pagamento"
-                            value="dinheiro"
-                            style={{ display: "none"}}
-                            checked={formaPagamento === "dinheiro"}
-                            onChange={() => handleSelect("dinheiro")}
-                        />
+        {isLoading && <div className="fullscreen-loading" />}
+            <div className="metodos-pagamento-container">
+                <div className="formas-pagamentos">
+                    <div className="opcao-pagamento" >
+                        <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("pix")} >
+                            <IconePix onClick={() => ToogleVisibility("pix")}/>
+                            <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("pix")}>PIX</span>
+                            <input
+                                type="radio"
+                                name="pagamento"
+                                value="pix"
+                                style={{ display: "none"}}
+                                checked={formaPagamento === "pix"}
+                                onChange={() => handleSelect("pix")}
+                            />
+                        </label>
+                        {opcoesDisponiveis === "pix" && (<PagamentoPix />)}
                     </div>
-                    {opcoesDisponiveis === "dinheiro" && (<PagamentoDinheiro />)}
+                <div className="opcao-pagamento" >
+                        <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("credito")}>
+                            <IconeCredito onClick={() => ToogleVisibility("credito")}/>
+                            <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("credito")}>CRÉDITO</span>
+                            <input
+                                type="radio"
+                                name="pagamento"
+                                value="credito"
+                                style={{ display: "none"}}
+                                checked={formaPagamento === "credito"}
+                                onChange={() => handleSelect("credito")}
+                            />
+                        </label>
+                        {opcoesDisponiveis === "credito" && (<PagamentoCredito />)}
+                    </div>
+                    <div className="opcao-pagamento" >
+                        <label className="checkbox-pagamentos" onClick={() => ToogleVisibility("debito")}>
+                            <IconeDebito onClick={() => ToogleVisibility("debito")} />
+                            <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("debito")}>DÉBITO</span>
+                            <input
+                                type="radio"
+                                name="pagamento"
+                                value="debito"
+                                style={{ display: "none"}}
+                                checked={formaPagamento === "debito"}
+                                onChange={() => handleSelect("debito")}
+                            />
+                        </label>
+                        {opcoesDisponiveis === "debito" && (<PagamentoDebito />)}
+                    </div>
+                    <div className="opcao-pagamento" >
+                        <div className="checkbox-pagamentos" onClick={() => ToogleVisibility("dinheiro")}>
+                            <IconeDinheiro onClick={() => ToogleVisibility("dinheiro")}/>
+                            <span className="label-checkbox-pagamentos" onClick={() => ToogleVisibility("dinheiro")}>DINHEIRO</span>
+                            <input
+                                type="radio"
+                                name="pagamento"
+                                value="dinheiro"
+                                style={{ display: "none"}}
+                                checked={formaPagamento === "dinheiro"}
+                                onChange={() => handleSelect("dinheiro")}
+                            />
+                        </div>
+                        {opcoesDisponiveis === "dinheiro" && (<PagamentoDinheiro />)}
+                    </div>
+                </div>
+                <div style={{"width":"100%"}}>
+                    <div className="container-total-geral" style={{marginBottom: "20px"}}>
+                        <h3 style={{"color": "white"}}>TOTAL GERAL</h3>
+                        <h3 className="value-total-geral"><label>R$</label>{valor_total.toFixed(2)}</h3>
+                    </div>
+                    <div className="Botoes-Checkout">
+                        <Link to="/Checkout/Etapa2">
+                            <ButtonBack/>
+                        </Link>
+                        <button
+                            onClick={handledSubmit}
+                            className={`send 
+                                ${isLoading ? "is-loading" : ""} 
+                                ${isSuccess ? "is-success" : ""}`}
+                        >
+                            <svg viewBox="0 0 90.594 59.714">
+                                <polyline
+                                    className="check"
+                                    fill="none"
+                                    stroke="#FFFFFF"
+                                    strokeWidth="10"
+                                    strokeMiterlimit="10"
+                                    points="1.768,23.532 34.415,56.179 88.826,1.768"
+                                />
+                            </svg>
+                            <span>Send</span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div style={{"width":"100%"}}>
-                <div className="container-total-geral" style={{marginBottom: "20px"}}>
-                    <h3 style={{"color": "white"}}>TOTAL GERAL</h3>
-                    <h3 className="value-total-geral"><label>R$</label>{valor_total.toFixed(2)}</h3>
-                </div>
-                <div className="Botoes-Checkout">
-                    <Link to="/Checkout/Etapa2">
-                        <ButtonBack/>
-                    </Link>
-                    <button onClick={handledSubmit} id="send" className="send">
-                        <svg viewBox="0 0 90.594 59.714">
-                            <polyline className="check" fill="none" stroke="#FFFFFF" stroke-width="10" stroke-miterlimit="10" points="1.768,23.532 34.415,56.179 88.826,1.768"/>
-                        </svg>
-                        <span>Send</span>
-                    </button>
-                </div>
-            </div>
-        </div>
         </>
     )
 
